3aca6a289229501210cf4fbf69a0b6375e846e07
Kavi Varsha <kavivarsha23cs@rnsit.ac.in>
2024-10-21 19:27:14 +0530

    Sum of array elements using recursion

--- C programs/sum_of_array.c
+++ C programs/sum_of_array.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
-int sum_of_array(int n, int a[]){
-    sum[0]=a[0];
-    if (n==0){
+
+// Recursive function to calculate the sum of array elements
+int sum_of_array(int n, int a[]) {
+    // Base case: if n is 0, return the first element
+    if (n == 0) {
         return a[0];
-    }
-    else{
-        return a[n]+sum_of_array(n-1,a[n-1]);
+    } 
+    // Recursive case: return current element plus the sum of previous elements
+    else {
+        return a[n] + sum_of_array(n - 1, a);
     }
 }
-int main(){
+
+int main() {
     int n, a[20];
-    printf("Enter a value : ");
-    scanf("%d",&n);
-    printf("Enter the elements : ");
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
+    // Prompt user to enter the number of elements in the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    // Prompt user to enter the array elements
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
-    printf("Sum of array elements : %d \n",n,sum_of_array(n,a));
+    
+    // Print the sum of array elements
+    printf("Sum of array elements: %d\n", sum_of_array(n - 1, a)); // n-1 because if n=5 then array last index will be a[4]
     
     return 0;
 }
