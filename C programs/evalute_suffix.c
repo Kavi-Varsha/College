3dcb1675724fa3660cbd84e4def606181a17ce0b
Kavi Varsha <kavivarsha23cs@rnsit.ac.in>
2024-11-06 21:46:43 +0530

    Dynamic allocation of circular queues
    Evaluation of suffix expression using stacks
    Conversion of infix to postfix expression using stacks

---
+++ C programs/evalute_suffix.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+double compute(double op1,double op2, char symbol){
+    switch(symbol){
+        case '+':return op1+op2;
+        case'-':return op1-op2;
+        case'*':return op1*op2;
+        case'/':return op1/op2;
+        case'^':
+        case'$':return pow(op1,op2);
+    }
+}
+
+int main(){
+    char symbol,suffix[30];
+    int n,i;
+    double s[78];
+    double op1,op2,res;
+    int top=-1;
+    printf("Enter suffix expression : ");
+    scanf("%s",suffix);
+    for(i=0;i<strlen(suffix);i++){
+        symbol=suffix[i];
+        if(isdigit(symbol)){
+            s[++top]=symbol-'0';
+        }
+        else{
+            op2=s[top--];
+            op1=s[top--];
+            res=compute(op1,op2,symbol);
+            s[++top]=res;
+
+        }
+        
+    }
+    res=s[top--];
+    printf("Result : %lf",res);
+    return 0;
+}
---
+++ C programs/infix_postfix.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+int f(char symbol){
+    switch(symbol){
+        case '+':
+        case '-':return 2;
+        case '*':
+        case '/': return 4;
+        case '^':
+        case '$': return 5;
+        case'(': return 0;
+        case '#': return -1;
+        default: return 8;
+    }
+}
+
+int g(char symbol){
+    switch(symbol){
+        case '+':
+        case '-':return 1;
+        case '*':
+        case '/': return 3;
+        case '^':
+        case '$': return 6;
+        case'(': return 9;
+        case ')':return 0;
+        default: return 7;
+    }
+}
+
+void infix_postfix(char infix[],char postfix[]){
+    int top=-1;
+    char symbol,s[30];
+    int i,j=0;
+    s[top++]='#';
+    for(i=0;i<strlen(infix);i++){
+        symbol=infix[i];
+        while(f(s[top])>g(symbol)){
+            postfix[j++]=s[top--];
+        }
+        if(f(s[top])!=g(symbol)){
+            s[++top]=symbol;
+        }
+        else{
+            top--;
+        }
+    }
+    while(s[top]!='#'){
+        postfix[j++]=s[top--];
+    }
+    postfix[j]='\0';
+}
+
+void main(){
+    char postfix[30],infix[20];
+    printf("Enter infix expression : ");
+    scanf("%s",infix);
+    infix_postfix(infix,postfix);
+    printf("Postfix expression : %s",postfix);
+}
